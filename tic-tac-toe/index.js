35170d69ab4423426852fec16d8741c240084539
I-AM-ISHIZAKI <ALINAISHIZAKI@OUTLOOK.COM>
2022-09-11 20:47:02 -0400

    First commit

---
+++ tic-tac-toe/index.js
@@ -0,0 +1,105 @@
+const cells = document.querySelectorAll(".cell")
+const statusText = document.querySelector("#statusText");
+const reset = document.querySelector("#reset");
+const winConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+let options = ["", "", "", "", "", "", "", "", ""];
+let currentPlayer = "X";
+let running = false;
+
+intializeGame();
+
+function intializeGame(){
+    cells.forEach(cell => cell.addEventListener("click", cellClicked))
+    reset.addEventListener("click", restartGame);
+    statusText.textContent = `${currentPlayer}'s turn`;
+    running = true;
+}
+function cellClicked(){
+    const cellIndex = this.getAttribute("cellIndex")
+
+    if(options[cellIndex] != "" || !running) {
+        return;
+    } 
+
+    updateCell(this, cellIndex);
+    checkWinner();
+}
+function updateCell(cell, index){
+    options[index] = currentPlayer;
+    cell.textContent = currentPlayer;
+}
+function changePlayer(){
+    currentPlayer = (currentPlayer == "X") ? "O" : "X";
+    statusText.textContent = `${currentPlayer}'s turn`;
+
+}
+function checkWinner(){
+    let roundWon = false;
+
+    for(let i=0; i<winConditions.length; i++) {
+        const condition = winConditions[i];
+        const cellA = options[condition[0]];
+        const cellB = options[condition[1]];
+        const cellC = options[condition[2]];
+
+        if(cellA == "" || cellB == "" || cellC == ""){
+            continue;
+        }
+        if(cellA == cellB && cellB == cellC) {
+            roundWon = true;
+            break;
+        }
+    }
+
+    if(roundWon){
+        statusText.textContent = `${currentPlayer} wins!`
+        running = false;
+    }
+    else if(!options.includes("")){
+        statusText.textContent = `It's a Draw!`
+        running = false;
+    }
+    else{
+        changePlayer();
+    }
+}
+function restartGame(){
+    currentPlayer = "X";
+    options = ["", "", "", "", "", "", "", "", ""];
+    statusText.textContent = `${currentPlayer}'s turn`;
+    cells.forEach(cell => cell.textContent = "");
+    running = true;
+
+}
+
+
+
+
+// const topLeft =
+// document.querySelector('#topLeft');
+
+// topLeft.addEventListener('click', (event) => {
+    // topLeft.textContent = `X`;
+// });
+
+// const topMid =
+// document.getElementById('topMid');
+
+// topMid.addEventListener('click', (event) => {
+    // topMid.textContent = `O`;
+// });
+
+
+
+//targetId
+//check win state
+//if statements => if all of these boxs are ====, the game is over. 
